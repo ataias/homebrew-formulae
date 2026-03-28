da8bf656d84718e778641f03dee6f2311611ac7d
Ataias Reis <ataiasreis@gmail.com>
2026-03-28 15:49:50 -0300

    add zig master formula

---
+++ Formula/zig-master.rb
@@ -0,0 +1,31 @@
+class ZigMaster < Formula
+  desc "Zig programming language (master/nightly build)"
+  homepage "https://ziglang.org/"
+  license "MIT"
+  version "0.16.0-dev.3013+abd131e33"
+
+  conflicts_with "zig", because: "both install a `zig` binary"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/ataias/homebrew-formulae/releases/download/zig-master-latest/zig-aarch64-macos-0.16.0-dev.3013+abd131e33.tar.xz"
+      sha256 "f019abfcd40f817acfcf9cd856ace6c5ea5450f30bc2a3c659e6bfdd3b397fed"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/ataias/homebrew-formulae/releases/download/zig-master-latest/zig-x86_64-macos-0.16.0-dev.3013+abd131e33.tar.xz"
+      sha256 "7835cfe2096e7fbf31d318b87e56d2a17e1b2e67e1e2eec812fdf936ab28df66"
+    end
+  end
+
+  def install
+    bin.install "zig"
+    lib.install Dir["lib/*"]
+  end
+
+  test do
+    system bin/"zig", "init"
+    system bin/"zig", "build", "--summary", "all"
+    system bin/"zig", "build", "run"
+    system bin/"zig", "build", "test", "--summary", "all"
+  end
+end
